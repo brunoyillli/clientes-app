99347fe24b76b98350ae1d0a0f52e6393bf4da60
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-01 09:44:56 -0300

    ajuste de responsividade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
+
+  ngAfterViewInit(){
+    (function($) {
+      "use strict";
+  
+      var path = window.location.href; 
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this.getAttribute("href") === path) {
+                  $(this).addClass("active");
+              }
+          });
+  
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+  }
 }
