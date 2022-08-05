d515e83b74850589e456f6e38258b90756f7bff8
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-05 10:53:31 -0300

    deletar clientes

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -12,6 +12,9 @@ export class ClientesListaComponent implements OnInit {
 
   clientes?: Cliente[] = [];
   clienteSelecionado!: Cliente;
+  mensagemSucesso: String = "";
+  mensagemErro: String = "";
+
   constructor(private service: ClientesService, private router: Router) { 
 
   }
@@ -26,5 +29,17 @@ export class ClientesListaComponent implements OnInit {
 
   preparaDelecao(cliente: Cliente){
     this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+    this.service
+      .deletar(this.clienteSelecionado)
+      .subscribe( 
+        response => {
+          this.mensagemSucesso = "Cliente deletado com sucesso!",
+          this.ngOnInit();
+      },
+        erro => this.mensagemErro = "Ocorreu um erro ao deletar o cliente"
+      )
   }
 }
