925af006552ad1fa8861c8a1fbb0714b1893575d
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-08 10:36:56 -0300

    salvando prestador

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from 'src/app/clientes/cliente';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 import { ServicoPrestado } from '../servicoPrestado';
 
 @Component({
@@ -14,7 +15,8 @@ export class ServicoPrestadoFormComponent implements OnInit {
   servico!: ServicoPrestado;
 
   constructor(
-    private clienteService: ClientesService
+    private clienteService: ClientesService,
+    private service: ServicoPrestadoService
   ) {
     this.servico = new ServicoPrestado();
    }
@@ -26,6 +28,9 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.servico)
+    this.service.salvar(this.servico)
+    .subscribe( response => {
+      console.log(response)
+    })
   }
 }
