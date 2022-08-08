4b142eacaf9ef59914e68155250a0d119e13e35d
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-08 11:35:55 -0300

    mensagens de feedback prestador

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -13,6 +13,8 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
   servico!: ServicoPrestado;
+  success: boolean = false;
+  errors?: String[];
 
   constructor(
     private clienteService: ClientesService,
@@ -30,7 +32,12 @@ export class ServicoPrestadoFormComponent implements OnInit {
   onSubmit(){
     this.service.salvar(this.servico)
     .subscribe( response => {
-      console.log(response)
-    })
+      this.success = true;
+      this.errors = [];
+      this.servico = new ServicoPrestado();
+    } , errorResponse => {
+      this.success = false;
+      this.errors = errorResponse.error.errors;
+    });
   }
 }
