440d0d2f286fa71c32a35658fe00ff80198874d0
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-09 09:32:36 -0300

    finalizando pesquisa de servico prestado

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -21,7 +21,7 @@ export class ServicoPrestadoService {
   buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]>{
     const httpParams = new HttpParams()
         .set("nome", nome)
-        .set("mes", mes.toString());
+        .set("mes", mes ? mes.toString() : "");
 
     const url = this.apiURL + "?" + httpParams.toString();
     return this.http.get<any>(url);
