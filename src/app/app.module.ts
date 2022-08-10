aa45bcd50d670941cf4571fa0ce0c7d25d952579
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-10 08:50:09 -0300

    comecando formulario de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,18 @@ import { ClientesModule } from './clientes/clientes.module';
 import { ClientesService } from './clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
